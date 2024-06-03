207271826479d3d270dde57d9d11aee571729f6e
betselotaderaw <betselotaderaw@gmail.com>
2024-06-03 18:59:49 +0200

    contact us page

---
+++ app/contact/page.jsx
@@ -0,0 +1,9 @@
+import Contact from "../components/Contact";
+
+export default function ContactPage() {
+    return (
+        <div className="pl-32  flex justify-center">
+            <Contact/>
+        </div>
+    );
+}
